beac58e790361f53f02fadaa954d37709187b389
Alexey Yagelo <ayagelo@elinext.com>
2020-06-05 01:10:02 +0300

    Added HTML page

---
+++ js/builds.js
@@ -0,0 +1,343 @@
+var builds = [
+  {
+    "id": 1,
+    "name": "Ягин 1-ый ПК",
+    "lastUpdate": "2019-12-16",
+    "parts": [
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/cpu/intel/corei54670k",
+        "name": "Процессор Intel Core i5-4670K",
+        "price": 530
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/motherboard/asus/z87plus",
+        "name": "Материнская плата ASUS Z87-PLUS",
+        "price": 100
+      },
+      {
+        "count": 2,
+        "link": "https://catalog.onliner.by/dram/ncp/ncph10audr16m28",
+        "name": "Оперативная память NCP 8GB",
+        "price": 65
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/videocard/gigabyte/gvn980wf3oc4gd",
+        "name": "Видеокарта Gigabyte GeForce GTX 980 WindForce 3 OC",
+        "price": 480
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/ssd/samsung/mz75e500bw",
+        "name": "SSD Samsung 850 Evo 500GB",
+        "price": 175
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/hdd/seagate/sg_barrst2000dm0",
+        "name": "Жесткий диск Seagate BarraCuda 2TB",
+        "price": 177
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/wirelessadapter/tp_link/tptlwn781nd",
+        "name": "Wi-Fi адаптер TP-Link TL-WN781ND",
+        "price": 18
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/optical/liteon/liteonihas12214",
+        "name": "DVD привод Lite-On",
+        "price": 28
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/powersupply/chieftec/a80ctg750c750w",
+        "name": "Блок питания Chieftec A-80 CTG-750C 750W",
+        "price": 151
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/fan/deepcool/dc_gammaxxs40",
+        "name": "Кулер для процессора DeepCool GAMMAXX S40",
+        "price": 33
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/chassis/zalman/z3plus",
+        "name": "Корпус Zalman Z3 Plus",
+        "price": 99
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Ягин 2-ой ПК",
+    "lastUpdate": "2020-05-31",
+    "parts": [
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/cpu/intel/i78700k",
+        "name": "Процессор Intel Core i7-8700K",
+        "price": 913
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/motherboard/asus/primez370a",
+        "name": "Материнская плата ASUS Prime Z370-A",
+        "price": 420
+      },
+      {
+        "count": 4,
+        "link": "https://catalog.onliner.by/dram/hyperx/hx432c16fb3a8",
+        "name": "Оперативная память HyperX Fury RGB 8GB",
+        "price": 130
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/videocard/gigabyte/gvn208saorus8gc",
+        "name": "Видеокарта Gigabyte AORUS GeForce RTX 2080 Super",
+        "price": 1989
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/ssd/samsung/mzv7e500bw",
+        "name": "SSD Samsung 970 Evo 500GB",
+        "price": 264
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/ssd/samsung/mz76e1t0bw",
+        "name": "SSD Samsung 860 Evo 1TB",
+        "price": 365
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/hdd/seagate/st2000dm006",
+        "name": "Жесткий диск Seagate BarraCuda 2TB",
+        "price": 130
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/wirelessadapter/tp_link/archert6e",
+        "name": "Wi-Fi адаптер TP-Link Archer T6E",
+        "price": 73
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/wirelessadapter/asus/usbbt400",
+        "name": "Bluetooth адаптер ASUS",
+        "price": 37
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/powersupply/chieftec/gdp750c",
+        "name": "Блок питания Chieftec A-90 750W",
+        "price": 173
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/fan/nzxt/rlkrx5301",
+        "name": "Система водяного охлаждения NZXT Kraken X53",
+        "price": 364
+      },
+      {
+        "count": 5,
+        "link": "https://catalog.onliner.by/fan/bequiet/bl066",
+        "name": "Вентилятор для корпуса be quiet! Silent Wings 3 120mm PWM",
+        "price": 52
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/chassis/thermaltake/ca1h700m1wn00",
+        "name": "Корпус Thermaltake Versa C23 версия с закаленным стеклом RGB",
+        "price": 211
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Баскета ПК",
+    "lastUpdate": "2020-02-06",
+    "parts": [
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/cpu/amd/rzn53600",
+        "name": "Процессор AMD Ryzen 5 3600",
+        "price": 406
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/motherboard/gigabyte/b450aoruselite",
+        "name": "Материнская плата Gigabyte B450 Aorus Elite (rev. 1.0)",
+        "price": 211
+      },
+      {
+        "count": 2,
+        "link": "https://catalog.onliner.by/dram/crucial/bls8g4d32aesbk",
+        "name": "Оперативная память Crucial Ballistix Sport LT 8GB",
+        "price": 96
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/videocard/gigabyte/gvn165swf2oc4gd",
+        "name": "Видеокарта Gigabyte GeForce GTX 1650 Super",
+        "price": 411
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/ssd/kingston/sa400s37240g",
+        "name": "SSD Kingston A400 240GB",
+        "price": 78
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/hdd/seagate/st500lm030",
+        "name": "Жесткий диск Seagate Barracuda 500GB",
+        "price": 86
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/powersupply/chieftec/protonbdf650c",
+        "name": "Блок питания Chieftec Proton BDF-650C",
+        "price": 143
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/fan/pccooler/gix6r",
+        "name": "Кулер для процессора PCCooler GI-X6R",
+        "price": 55
+      },
+      {
+        "count": 3,
+        "link": "https://catalog.onliner.by/fan/arcticcooling/arcticf12",
+        "name": "Вентилятор для корпуса Arctic F12",
+        "price": 13
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/chassis/zalman/zalmans2",
+        "name": "Корпус Zalman S2",
+        "price": 70
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Димаса ПК",
+    "lastUpdate": "2019-12-16",
+    "parts": [
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/cpu/intel/i58600k",
+        "name": "Процессор Intel Core i5-8600K",
+        "price": 570
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/motherboard/asus/primez370p",
+        "name": "Материнская плата ASUS PRIME Z370-P",
+        "price": 316
+      },
+      {
+        "count": 2,
+        "link": "https://catalog.onliner.by/dram/crucial/ct8g4dfd824a",
+        "name": "Оперативная память Crucial 8GB",
+        "price": 74
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/videocard/gigabyte/gvn1080g1gaming8",
+        "name": "Видеокарта Gigabyte GeForce GTX 1080 G1 Gaming 8GB",
+        "price": 700
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/ssd/samsung/mzv7s500bw",
+        "name": "SSD Samsung 970 Evo Plus 500GB",
+        "price": 246
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/hdd/seagate/st1000dm010",
+        "name": "Жесткий диск Seagate BarraCuda 1TB",
+        "price": 78
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/powersupply/chieftec/protonbdf650c",
+        "name": "Блок питания Chieftec A-80 CTG-750C 750W",
+        "price": 129
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/fan/deepcool/gammaxx300",
+        "name": "Кулер для процессора DeepCool GAMMAXX 300",
+        "price": 33
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/chassis/zalman/zalmanz3",
+        "name": "Корпус Zalman Z3",
+        "price": 89
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Егора ПК",
+    "lastUpdate": "2020-06-04",
+    "parts": [
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/cpu/amd/rzn53600",
+        "name": "Процессор AMD Ryzen 5 3600",
+        "price": 392
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/motherboard/asus/tufb450progaming",
+        "name": "Материнская плата ASUS TUF B450-Pro Gaming",
+        "price": 283
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/dram/hyperx/hx436c17fb3k216",
+        "name": "Оперативная память HyperX Fury 2x8GB",
+        "price": 258
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/videocard/gigabyte/gvn166sgamingoc6",
+        "name": "Видеокарта Gigabyte GeForce GTX 1660 Super",
+        "price": 726
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/ssd/crucial/ct1000p1ssd8",
+        "name": "SSD Crucial P1 1TB",
+        "price": 275
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/powersupply/chieftec/pps650fc",
+        "name": "Блок питания Chieftec Polaris PPS-650FC",
+        "price": 202
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/fan/pccooler/gix5r",
+        "name": "Кулер для процессора PCCooler GI-X5R",
+        "price": 55
+      },
+      {
+        "count": 1,
+        "link": "https://catalog.onliner.by/chassis/zalman/zalmanx3",
+        "name": "Корпус Zalman X3",
+        "price": 223
+      }
+    ]
+  }
+];
